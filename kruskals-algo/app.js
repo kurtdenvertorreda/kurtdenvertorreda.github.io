bc7463e3a6ecc078ef355dc39cea9cc78ddafe9c
SpaceMonkey002 <138639636+SpaceMonkey002@users.noreply.github.com>
2023-09-25 15:50:22 +0800

    updated mst

---
+++ kruskals-algo/app.js
@@ -0,0 +1,72 @@
+// SETUP
+import * as dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const	app 	= express(),
+		port	= 3000 || process.env.PORT;
+
+
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// ROUTES
+app.get("/", (req, res) => {
+	try {
+		res.render("home");
+	} catch(err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/map", (req, res) => {
+	try {
+		const data = JSON.parse(decodeURIComponent(req.query.data || "{}"));
+		res.render("mapPage", {data: data});
+	} catch (err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/about", (req, res) => {
+	try {
+		res.render("about");
+	} catch (err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/contact", (req, res) => {
+	try {
+		res.render("contact");
+	} catch (err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/getData", (req, res) => {
+	try {
+		let data = req.query;
+		console.log(data);
+		res.redirect(`/map?data=${encodeURIComponent(JSON.stringify(data))}`);
+	} catch (err) {
+		res.send("error");
+		console.log(err);
+	}
+});
+
+app.get("*", (req, res) => {
+	res.render("errorpage");
+})
+
+
+// LISTEN
+app.listen(port, () => {
+	console.log(`Server starting on port ${port}`);
+})
